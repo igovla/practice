adfc0b3f5ad4597e80304f212b02d48b7a9dc6ba
igor <igor@babkux.(none)>
2013-11-02 13:01:28 +0400

    Ещё одно исправление Travis

--- practice_4/views.py
+++ practice_4/views.py
@@ -36,12 +36,12 @@ def authors(request):
 def listing(request, diry):
     lidir = []
     for fd in os.listdir('/var/log/' + str(diry)):
-      if os.path.isdir('/var/log/' + str(diry) + '/' + fd):
-        templ = '<tr><td><a href="' + str(fd) + '/">' + fd + '</a></td>'
-      else:
-        templ = '<tr><td>' + fd + '</td>'
-      templ = templ + '<td>' + str(os.path.getsize('/var/log/' + str(diry) + '/' + fd)) + '</td>'
-      templ = templ + '<td>' + str(time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime(os.path.getmtime('/var/log/' + str(diry) + '/' + fd)))) + '</td></tr>'
-      lidir.append(templ)
+        if os.path.isdir('/var/log/' + str(diry) + '/' + fd):
+            templ = '<tr><td><a href="' + str(fd) + '/">' + fd + '</a></td>'
+        else:
+            templ = '<tr><td>' + fd + '</td>'
+        templ = templ + '<td>' + str(os.path.getsize('/var/log/' + str(diry) + '/' + fd)) + '</td>'
+        templ = templ + '<td>' + str(time.strftime("%a, %d %b %Y %H:%M:%S", time.localtime(os.path.getmtime('/var/log/' + str(diry) + '/' + fd)))) + '</td></tr>'
+        lidir.append(templ)
     context = {u'dir_content': lidir, }
     return render(request, 'listing.html', context)
